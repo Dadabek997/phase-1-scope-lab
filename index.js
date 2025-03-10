3d69c43f937f749dfda9aed1bb06c6565729da1a
Dadabek Rajabov <barajabov@gmail.com>
2025-03-09 21:08:02 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+    console.log(customerName)
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+console.log('not bob' === undefined)
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Jeff'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not Jeff'
+}
